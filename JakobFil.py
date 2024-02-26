5317ef0e0c39cfecdbd6c40e7dbc393d6d038e66
Jakob K. Pedersen <37065647+BaconToast420@users.noreply.github.com>
2024-02-26 12:30:36 +0100

    Jeg lavede noget i min fil :)

---
+++ JakobFil.py
@@ -0,0 +1,16 @@
+import librosa
+import os
+import librosa.feature
+import matplotlib.pyplot as plt
+import pandas
+
+
+class SoundFile:
+    def __init__(self, path):
+        self.path = path
+
+
+
+directory = r'C:\Users\jakob\Desktop\JakobLytteLyde'
+
+bruhListe = []
