5984157c194694633a1236da3b4de14ef6e1ad1a
Jakob K. Pedersen <37065647+BaconToast420@users.noreply.github.com>
2024-02-27 14:00:43 +0100

    Jakob har skrevet noget der nok ikke virker

--- JakobFil.py
+++ JakobFil.py
@@ -21,17 +21,17 @@ class SoundFile:
         self.metallic = metallic
 
 
-sound1 = SoundFile(r'C:\Users\jakob\Desktop\JakobLytteLyde\004400-rdd_kick718.wav', "sound1", False, False, True, True, True, False, False, False, False, False)
-sound2 = SoundFile(r'C:\Users\jakob\Desktop\JakobLytteLyde\004401-rdd_kick719.wav', "sound2", False, False, False, True, False, False, False, False, False, False)
-sound3 = SoundFile(r'C:\Users\jakob\Desktop\JakobLytteLyde\004403-rdd_kick720.wav', "sound3", False, False, False, True, True, False, False, False, False, False)
-sound4 = SoundFile(r'C:\Users\jakob\Desktop\JakobLytteLyde\004404-rdd_kick721.wav', "sound4", False, False, True, True, True, False, True, False, False, False)
-sound5 = SoundFile(r'C:\Users\jakob\Desktop\JakobLytteLyde\004405-rdd_kick722.wav', "sound5", False, False, True, True, True, True, True, False, False, False)
-sound6 = SoundFile(r'C:\Users\jakob\Desktop\JakobLytteLyde\004406-rdd_kick723.wav', "sound6", False, False, True, False, True, False, True, False, True, False)
-sound7 = SoundFile(r'C:\Users\jakob\Desktop\JakobLytteLyde\004407-rdd_kick724.wav', "sound7", False, True, True, False, True, False, False, False, False, False)
-sound8 = SoundFile(r'C:\Users\jakob\Desktop\JakobLytteLyde\004408-rdd_kick725.wav', "sound8", False, False, True, False, True, False, True, False, True, False)
-sound9 = SoundFile(r'C:\Users\jakob\Desktop\JakobLytteLyde\004409-rdd_kick726.wav', "sound9", False, False, True, True, True, True, False, False, False, False)
-sound10 = SoundFile(r'C:\Users\jakob\Desktop\JakobLytteLyde\004410-rdd_kick727.wav', "sound10", False, False, False, True, True, False, False, False, False, False)
-sound11 = SoundFile(r'C:\Users\jakob\Desktop\JakobLytteLyde\004411-rdd_kick728.wav', "sound11", False, False, True, False, False, False, False, False, True, False)
+sound1 = SoundFile('/JakobLyde/004400-rdd_kick718.wav', "sound1", False, False, True, True, True, False, False, False, False, False)
+sound2 = SoundFile('/JakobLyde/004401-rdd_kick719.wav', "sound2", False, False, False, True, False, False, False, False, False, False)
+sound3 = SoundFile('/JakobLyde/004403-rdd_kick720.wav', "sound3", False, False, False, True, True, False, False, False, False, False)
+sound4 = SoundFile('/JakobLyde/004404-rdd_kick721.wav', "sound4", False, False, True, True, True, False, True, False, False, False)
+sound5 = SoundFile('/JakobLyde/004405-rdd_kick722.wav', "sound5", False, False, True, True, True, True, True, False, False, False)
+sound6 = SoundFile('/JakobLyde/004406-rdd_kick723.wav', "sound6", False, False, True, False, True, False, True, False, True, False)
+sound7 = SoundFile('/JakobLyde/004407-rdd_kick724.wav', "sound7", False, True, True, False, True, False, False, False, False, False)
+sound8 = SoundFile('/JakobLyde/004408-rdd_kick725.wav', "sound8", False, False, True, False, True, False, True, False, True, False)
+sound9 = SoundFile('/JakobLyde/004409-rdd_kick726.wav', "sound9", False, False, True, True, True, True, False, False, False, False)
+sound10 = SoundFile('/JakobLyde/004410-rdd_kick727.wav', "sound10", False, False, False, True, True, False, False, False, False, False)
+sound11 = SoundFile('/JakobLyde/004411-rdd_kick728.wav', "sound11", False, False, True, False, False, False, False, False, True, False)
 
 directory = r'C:\Users\jakob\Desktop\JakobLytteLyde'
 
