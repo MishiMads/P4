e1334b5bef0cc7c83a4c901c4c4caedc80531587
Jakob K. Pedersen <37065647+BaconToast420@users.noreply.github.com>
2024-03-08 14:16:18 +0100

    Jakob har ingen ide om hvad han laver

--- JakobTSNE2.py
+++ JakobTSNE2.py
@@ -6,4 +6,20 @@ import pandas
 from sklearn.manifold import TSNE
 import numpy as np
 
-sound_directory = r'C:\Users\jakob\Desktop\A_kicks\subdir1'
+sound_directory = r'C:\Users\jakob\Desktop\100_sounds'
+files = []
+for file in sound_directory:
+    files.append(file)
+print(files)
+
+
+sounds = []
+
+for file_name in os.listdir(sound_directory):
+    if file_name.endswith('.wav'):
+        file_path = os.path.join(sound_directory, file_name)
+        y, sr = librosa.load(file_path)
+        sounds.append((y, sr))
+
+#tsne = TSNE(n_components=2)
+#reduced_features_tsne = tsne.fit_transform(sounds)
