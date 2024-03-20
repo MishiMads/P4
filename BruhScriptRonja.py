64cf110eac4471ecd6000cd94f1fdb335d971bd5
SoupFanAccount <112694021+SoupFanAccount@users.noreply.github.com>
2024-03-20 15:24:07 +0100

    Har gjort så det ikke åbner mediaplayer

--- BruhScriptRonja.py
+++ BruhScriptRonja.py
@@ -10,7 +10,7 @@ import mplcursors
 import tkinter as tk
 from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import soundfile as sf
+import sounddevice as sd
 
 # Calculates distance between points
 def euclidean_distance(point1, point2):
@@ -21,9 +21,9 @@ def play_sound(selected_point):
     selected_index = np.argmin([euclidean_distance(selected_point, point) for point in pca_results])
     selected_filename = features_dataframe.iloc[selected_index]['Filename']
     selected_file_path = os.path.join(drumFolder, selected_filename)
-    y, sr = librosa.load(selected_file_path)
-    sf.write("temp.wav", y, sr)
-    os.system("start temp.wav")
+    y, sr = librosa.load(selected_file_path, sr=None)
+    sd.play(y, sr)
+    sd.wait()
 
 # Assuming the drumFolder is correctly set to where your .wav files are located
 drumFolder = '500_Sounds'
@@ -73,31 +73,23 @@ pca_results = pca.fit_transform(scaled_features)
 # Identify important axes (features) for PCA by looking at the explained variance ratio
 print(f"Explained variance by component: {pca.explained_variance_ratio_}")
 
-# Plotting PCA results
-#plt.figure(figsize=(7, 5))
-#plt.scatter(pca_results[:, 0], pca_results[:, 1], alpha=0.5)
-#plt.title('PCA Results on Spread-Out Log-transformed Features')
-#plt.xlabel('PCA Component 1')
-#plt.ylabel('PCA Component 2')
-#plt.colorbar(label='Sample Label')
-
-root = tk.Tk()
-root.title("PCA Results")
+# Create window
+window = tk.Tk()
+window.title("PCA Results")
 
 fig, ax = plt.subplots(figsize=(7,5))
 scatter = ax.scatter(pca_results[:, 0], pca_results[:, 1], alpha=0.5)
 ax.set_title('PCA Results on Spread-Out Log-Transformed Features')
 ax.set_xlabel('PCA Component 1')
 ax.set_ylabel('PCA Component 2')
-#ax._colorbars(label='Sample Label')
 
 mplcursors.cursor(scatter).connect("add", lambda sel: play_sound(sel.target))
 
-canvas = FigureCanvasTkAgg(fig, master=root)
+canvas = FigureCanvasTkAgg(fig, master=window)
 canvas.draw()
 canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-root.mainloop()
+window.mainloop()
 
 #plt.show()
 
