38c0e4ec7a81ca31a1d765dea97de2857c0a3a5b
MishiMads <mnje16@student.aau.dk>
2024-03-21 13:29:17 +0100

    Lowkey push

--- Mads2Dimensioner.py
+++ Mads2Dimensioner.py
@@ -9,6 +9,7 @@ import tkinter as tk
 from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import sounddevice as sd
+from matplotlib.text import Annotation
 
 # Calculates distance between points
 def euclidean_distance(point1, point2):
@@ -21,6 +22,7 @@ def play_sound(selected_point):
     selected_filename = features_dataframe.iloc[selected_index]['Filename']
     selected_file_path = os.path.join(drumFolder, selected_filename)
     y, sr = librosa.load(selected_file_path, sr=None)
+    sd.stop()  # Stop any currently playing sounds
     sd.play(y, sr)
     sd.wait()
 
@@ -35,11 +37,19 @@ for filename in os.listdir(drumFolder):
         # Extract selected features
         rms_energy = librosa.feature.rms(y=y).mean()
         spectral_flatness = librosa.feature.spectral_flatness(y=y).mean()
+        rms_energy = librosa.feature.rms(y=y).mean()
+        zero_crossing_rate = librosa.feature.zero_crossing_rate(y=y).mean()
+        onset_strength = librosa.onset.onset_strength(y=y, sr=sr).mean()
+        spectral_rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr).mean()
+        spectral_contrast = librosa.feature.spectral_contrast(y=y, sr=sr).mean()
 
         features_list.append({
             'Filename': filename,
             'RMS Energy': rms_energy,
-            'Spectral Flatness': spectral_flatness,
+            'Zero Crossing Rate': zero_crossing_rate,
+            #'Onset Strength': onset_strength,
+            #'Spectral Rolloff': spectral_rolloff,
+            #'Spectral Contrast': spectral_contrast,
         })
 
 # Convert features_list to a DataFrame
@@ -58,12 +68,44 @@ window = tk.Tk()
 window.title("Feature Scatter Plot")
 
 fig, ax = plt.subplots(figsize=(7,5))
-scatter = ax.scatter(scaled_features[:, 0], scaled_features[:, 1], alpha=0.5)
+scatter = ax.scatter(scaled_features[:, 0], scaled_features[:, 1], alpha=0.5, picker=5)  # Enable picker with a tolerance
+
 ax.set_title('Scatter Plot of RMS Energy and Spectral Flatness')
 ax.set_xlabel('Scaled RMS Energy')
 ax.set_ylabel('Scaled Spectral Flatness')
 
-mplcursors.cursor(scatter).connect("add", lambda sel: play_sound(np.array([sel.target[0], sel.target[1]])))
+# Initialize a text annotation for displaying filenames, but set it to be invisible for now
+tooltip = ax.annotate("", xy=(0,0), xytext=(20,20), textcoords="offset points",
+                      bbox=dict(boxstyle="round", fc="w"),
+                      arrowprops=dict(arrowstyle="->"))
+tooltip.set_visible(False)
+
+def on_pick(event):
+    if event.artist == scatter:
+        ind = event.ind[0]  # Get the index of the clicked point
+        selected_point = np.array([scaled_features[ind, 0], scaled_features[ind, 1]])
+        play_sound(selected_point)
+
+def update_tooltip(event):
+    vis = tooltip.get_visible()
+    if event.inaxes == ax:
+        contains, ind = scatter.contains(event)
+        if contains:
+            index = ind['ind'][0]
+            x, y = scatter.get_offsets()[index]
+            tooltip.xy = (x, y)
+            tooltip.set_text(features_dataframe['Filename'].iloc[index])
+            tooltip.set_visible(True)
+            fig.canvas.draw_idle()
+        else:
+            if vis:
+                tooltip.set_visible(False)
+                fig.canvas.draw_idle()
+
+fig.canvas.mpl_connect('pick_event', on_pick)
+
+# Comment this out to disable the tooltip :)
+fig.canvas.mpl_connect("motion_notify_event", update_tooltip)
 
 canvas = FigureCanvasTkAgg(fig, master=window)
 canvas.draw()
