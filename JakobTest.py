a7233f4ac2f1879759e2ddf310f46f77bea41822
Jakob K. Pedersen <37065647+BaconToast420@users.noreply.github.com>
2024-03-26 10:18:14 +0100

    mellemgem

--- JakobTest.py
+++ JakobTest.py
@@ -9,9 +9,12 @@ import numpy as np
 import tkinter as tk
 from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import mplcursors
+import sounddevice as sd
 
 
 sound_dir = '500_Sounds'
+feature_list = []
 
 
 def extract_feature1(audio):
@@ -35,13 +38,24 @@ for file in sound_files:
     X.append(feature1)
     feature2 = extract_feature2(audio_file)
     Y.append(feature2)
+    feature_list.append([feature1, feature2])
 
 X_log = np.log(X)
 Y_log = np.log(Y)
 
+# plt.figure(figsize=(7, 5))
+# plt.scatter(X, Y, alpha=0.5)
+# plt.title('2 Features')
+# plt.xlabel('Feature 1')
+# plt.ylabel('Feature 2')
+
+pca = PCA(n_components=2)
+reduced_features = pca.fit_transform(feature_list)
+
 plt.figure(figsize=(7, 5))
-plt.scatter(X_log, Y_log, alpha=0.5)
-plt.title('2 Features')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
+plt.scatter(reduced_features[:, 0], reduced_features[:, 1], alpha=0.5)
+plt.title('Plot')
+plt.xlabel('Axis 1')
+plt.ylabel('Axis 2')
+
 plt.show()
